c3c087540c70b38dd6448ba8392a173bb9ff46e5
Ins44ne <ins44ne91@gmail.com>
2024-07-19 21:52:19 +0300

    feat: add loader for html components

---
+++ withoutWebpack/js/loaderHTML.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+  function loadHtml(url, elementId) {
+    fetch(url)
+      .then((response) => response.text())
+      .then((data) => {
+        document.getElementById(elementId).innerHTML = data
+      })
+      .catch((error) => console.error('Error loading HTML:', error))
+  }
+
+  function loadSvg(url, elementId) {
+    fetch(url)
+      .then((response) => response.text())
+      .then((data) => {
+        document.getElementById(elementId).insertAdjacentHTML('beforeend', data)
+      })
+      .catch((error) => console.error('Error loading SVG:', error))
+  }
+
+  loadHtml('./pages/components/header.html', 'header')
+  loadHtml('./pages/components/footer.html', 'footer')
+  loadSvg('./img/icons/footer/facebook.svg', 'facebook')
+  loadSvg('./img/icons/footer/instagram.svg', 'instagram')
+  loadSvg('./img/icons/footer/linkedIn.svg', 'linkedIn')
+  loadSvg('./img/icons/footer/x.svg', 'x')
+  loadSvg('./img/icons/footer/youtube.svg', 'youtube')
+})
