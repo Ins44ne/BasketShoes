f8ba03d1e777e06ae1a8e02b02d193afaf5eb998
Ins44ne <ins44ne91@gmail.com>
2024-07-29 19:37:14 +0300

    feat: add functions for catalog page

--- withoutWebpack/js/catalogSwitchers.js
+++ withoutWebpack/js/catalogSwitchers.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const iconSwitchers = document.querySelectorAll('.icon-switcher')
-  iconSwitchers.forEach(function (iconSwitcher) {
-    iconSwitcher.addEventListener('click', function () {
+  function toggleIconSwitcher(event) {
+    const iconSwitcher = event.currentTarget.querySelector('.icon-switcher')
+    if (iconSwitcher) {
       iconSwitcher.classList.toggle('active')
-    })
+    }
+
+    const targetId = event.currentTarget.getAttribute('data-target')
+    if (targetId) {
+      const targetElement = document.getElementById(targetId)
+      if (targetElement) {
+        targetElement.classList.toggle('hide-item')
+        targetElement.classList.toggle('show-item')
+      }
+    }
+  }
+
+  const filterBlocks = document.querySelectorAll('.filter')
+  filterBlocks.forEach(function (filterBlock) {
+    filterBlock.addEventListener('click', toggleIconSwitcher)
   })
 })
